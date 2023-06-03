f3af781ee80a134bbc70248c2bd4076c4daf7515
Shubh Jani <sjani5@myseneca.ca>
2023-06-02 20:14:29 -0400

    commit

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ I declare that this assignment is my own work in accordance with Seneca  Academi
 Name: Shubh Jani 
 Student ID: 153660212 
 Date: 2 June 2023
-Cyclic Web App URL: _______________________________________________________
-GitHub Repository URL: ______________________________________________________
+Cyclic Web App URL:  https://long-puce-cobra-hose.cyclic.app
+GitHub Repository URL: https://github.com/sjani5/web322-app.git
 
 ********************************************************************************/ 
 
