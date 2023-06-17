3806347a4e86ba4de82738eb2d154278d2151d8e
Shubh Jani <sjani5@myseneca.ca>
2023-06-16 20:16:44 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,75 +1,180 @@
 /*********************************************************************************
 
-WEB322 – Assignment 02
+WEB322 – Assignment 03
 I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 
 Name: Shubh Jani 
 Student ID: 153660212 
-Date: 2 June 2023
+Date: 16 June 2023
 Cyclic Web App URL:   https://cute-teal-beaver-garb.cyclic.app
 GitHub Repository URL: https://github.com/sjani5/web322-app.git
 
 ********************************************************************************/ 
 
+const express = require('express');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+const store_service = require('./store-service');
+const upload = multer({ dest: 'uploads/'}); // no { storage: storage }
+const app = express();
+const port = process.env.PORT || 8080;
 
-const express = require('express')
-const store_service = require('./store-service')
-const app = express()
-const port = process.env.PORT || 8080
+cloudinary.config({
+  cloud_name: 'dnec2tgvq',
+  api_key: '244459785948447',
+  api_secret: 'JS0jza-oPnXSVUJEzn9qc1lrDTk',
+  secure: true
+});
 
-app.use(express.static('public')); 
+
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+// Route to serve "/views/addItem.html"
+app.get('/items/add', (req, res) => {
+  res.sendFile(__dirname + "/views/addItem.html");
+});
+
+app.post('/items/add', upload.single('featureImage'), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    upload(req)
+      .then((uploaded) => {
+        processItem(uploaded.url);
+      })
+      .catch((error) => {
+        console.log(error);
+        processItem("");
+      });
+  } else {
+    processItem("");
+  }
+
+  function processItem(imageUrl) {
+    req.body.featureImage = imageUrl;
+
+    // TODO: Process the req.body and add it as a new Item before redirecting to /items
+    store_service.addItem(req.body)
+      .then(() => {
+        res.redirect('/items');
+      })
+      .catch((error) => {
+        console.log(error);
+        res.redirect('/items');
+      });
+  }
+});
+
 app.get('/', (req, res) => {
-  res.redirect("/about")
+  res.redirect("/about");
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + "/views/about.html")
-  });
+  res.sendFile(__dirname + "/views/about.html");
+});
 
 app.get('/shop', (req, res) => {
-  store_service.getPublishedItems().then((data)=>{
-    res.json(data)
-  }).catch((err)=>{
-    return {'message': err}
-  })
+  store_service.getPublishedItems()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      return { 'message': err };
+    });
 });
 
 app.get('/items', (req, res) => {
-  store_service.getAllItems().then((data)=>{
-    res.json(data)
-  }).catch((err)=>{
-    return {'message': err}
-  })
+  const category = req.query.category;
+  const minDate = req.query.minDate;
+
+  if (category) {
+    store_service.getItemsByCategory(category)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err });
+      });
+  } else if (minDate) {
+    store_service.getItemsByMinDate(minDate)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err });
+      });
+  } else {
+    store_service.getAllItems()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err });
+      });
+  }
+});
+app.get('/item/:id', (req, res) => {
+  const itemId = parseInt(req.params.id);
+
+  store_service.getItemById(itemId)
+    .then((item) => {
+      if (item) {
+        res.json(item);
+      } else {
+        res.status(404).json({ message: 'Item not found' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 app.get('/categories', (req, res) => {
-    store_service.getCategories().then((data)=>{
-      res.json(data)
-    }).catch((err)=>{
-      return {'message': err}
+  store_service.getCategories()
+    .then((data) => {
+      res.json(data);
     })
-  });
+    .catch((err) => {
+      return { 'message': err };
+    });
+});
 
+app.get('*', (req, res) => {
+  res.send('Page not found, check URL', 404);
+});
 
-  app.get('*', function(req, res){
-    res.send('Page not found, check URL', 404);
-  });
-
-
-
-function onHTTPstart(){
-  console.log("server started on port: " + port)
+function onHTTPstart() {
+  console.log("Server started on port: " + port);
 }
 
-store_service.initialize().then(function(){
-  app.listen(port,onHTTPstart);
-}).catch(function(err){
-  console.log("unable to start" + err)
-})
+store_service.initialize()
+  .then(() => {
+    app.listen(port, onHTTPstart);
+  })
+  .catch((err) => {
+    console.log("Unable to start: " + err);
+  });
 
-
-app.use((req,res)=>{
-  res.status(404).send("Page does not exist")
-})
+app.use((req, res) => {
+  res.status(404).send("Page does not exist");
+});
--- store-service.js
+++ store-service.js
@@ -1,66 +1,121 @@
-const fs = require("fs"); 
+const fs = require("fs");
 
-items =[];
-categories = [];
+let items = [];
+let categories = [];
 
-module.exports.initialize = ()=>{
-return new Promise((resolve,reject)=>{
-    fs.readFile('./data/items.json', 'utf8', (err, data) => {
-        if (err){
+module.exports.initialize = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./data/items.json", "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        items = JSON.parse(data);
+        fs.readFile("./data/categories.json", "utf8", (err, data) => {
+          if (err) {
             reject(err);
-        }else{
-            items = JSON.parse(data);
-            fs.readFile('./data/categories.json','utf8',(err,data)=>{
-                if(err)
-                {
-                    reject(err)
-                }else{
-                    categories = JSON.parse(data);
-                    resolve();
-                }
-            })
-        }
-   });
-   })
-};  
-
-module.exports.getAllItems = () =>{
-    return new Promise((resolve,reject)=>{
-        if(items.length==0)
-        {
-            reject('items array is empty')
-        }else{
-            resolve(items)
-        }
-    })
-};
-
-module.exports.getPublishedItems= () =>{
-    return new Promise((resolve,reject)=>{
-        let pubItems = [];
-        for (let i=0;i<items.length;i++)
-        {
-            if(items[i].published==true)
-            {
-                pubItems.push(items[i]);
-            }
-        }
-        if(pubItems.length==0)
-        {
-            reject("no published items");
-        }else{
-            resolve(pubItems);
-        }
+          } else {
+            categories = JSON.parse(data);
+            resolve();
+          }
+        });
+      }
     });
-}
-
-module.exports.getCategories= () =>{
-    return new Promise((resolve,reject)=>{
-        if(categories.length==0)
-        {
-            reject('categories array is empty')
-        }else{
-            resolve(categories)
-        }
-    })
+  });
 };
+
+module.exports.getAllItems = () => {
+  return new Promise((resolve, reject) => {
+    if (items.length == 0) {
+      reject("Items array is empty");
+    } else {
+      resolve(items);
+    }
+  });
+};
+
+module.exports.getPublishedItems = () => {
+  return new Promise((resolve, reject) => {
+    let pubItems = [];
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].published == true) {
+        pubItems.push(items[i]);
+      }
+    }
+    if (pubItems.length == 0) {
+      reject("No published items");
+    } else {
+      resolve(pubItems);
+    }
+  });
+};
+
+module.exports.getCategories = () => {
+  return new Promise((resolve, reject) => {
+    if (categories.length == 0) {
+      reject("Categories array is empty");
+    } else {
+      resolve(categories);
+    }
+  });
+};
+
+module.exports.addItem = (itemData) => {
+  return new Promise((resolve, reject) => {
+    if (itemData.published === undefined) {
+      itemData.published = false;
+    } else {
+      itemData.published = true;
+    }
+    itemData.id = items.length + 1;
+    items.push(itemData);
+    resolve(itemData);
+  });
+};
+
+module.exports.getItemsByCategory = (category) => {
+  return new Promise((resolve, reject) => {
+    const filteredItems = items.filter((item) => item.category === category);
+    if (filteredItems.length === 0) {
+      reject("No results returned");
+    } else {
+      resolve(filteredItems);
+    }
+  });
+};
+
+module.exports.getItemById = (id) => {
+  return new Promise((resolve, reject) => {
+    const item = items.find((item) => item.id === id);
+    if (item) {
+      resolve(item);
+    } else {
+      reject("Item not found");
+    }
+  });
+};
+
+module.exports.getItemsByMinDate = (minDateStr) => {
+    return new Promise((resolve, reject) => {
+      const minDate = new Date(minDateStr);
+      const filteredItems = items.filter(
+        (item) => new Date(item.postDate) >= minDate
+      );
+      if (filteredItems.length === 0) {
+        reject("No results returned");
+      } else {
+        resolve(filteredItems);
+      }
+    });
+  };
+  
+  module.exports.getItemById = (id) => {
+    return new Promise((resolve, reject) => {
+      const item = items.find((item) => item.id === id);
+      if (item) {
+        resolve(item);
+      } else {
+        reject("No result returned");
+      }
+    });
+  };
+  
